0f129bc11b5a21684afd6520d3a758c19288556f
Michael MacNeill <98714598+Kingoftheslain@users.noreply.github.com>
2022-03-11 03:47:42 -0500

    Create First App.py
    Created a Simple app to begin learning

---
+++ First App/First App.py
@@ -0,0 +1,11 @@
+""" Name: Michael MacNeill
+    Date: March 11th, 2022
+    Following the Tutorial Book \"Python for Dummies.\""""
+
+quantity = 14
+
+unit_price = 26.99
+
+extended_price = quantity * unit_price
+
+print(round(extended_price, 2))
